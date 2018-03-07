c2a357bb7ba9fec3c7d4712ca2265f78adf6c0f0
Justin Chau <chausicle@gmail.com>
2018-03-06 19:14:25 -0800

    Add post, put, and delete routes

--- index.js
+++ index.js
@@ -20,17 +20,56 @@ let events = [
 ]
 
 app.get('/events', (req, res) => {
-  console.log('GET is working!')
   res.send(events)
 })
 
 app.get('/events/:uid', (req, res) => {
   if ((req.params.uid < 0 || req.params.uid > events.length - 1) || isNaN(req.params.uid)) {
-    console.log('KEEP TRYING')
     res.sendStatus(404)
   } else {
     res.send(events[req.params.uid])
   }
+})
+
+app.post('/events', (req, res) => {
+  let newEvent = {
+    uid: events.length,
+    title: 'New Event',
+    description: 'This is a new event!!',
+    date: '2020',
+    time: 'Next decade'
+  }
+  events.push(newEvent)
+  res.send(events)
+})
+
+app.put('/events/:uid', (req, res) => {
+  events.forEach((element, index) => {
+    if (Number(element.uid) === Number(req.params.uid)) {
+      events[index] = {
+        uid: Number(req.params.uid),
+        title: 'changed event',
+        description: 'updating',
+        date: 'TBD',
+        time: 'TBD'
+      }
+
+      res.send(events[req.params.uid])
+    }
+  })
+
+    res.sendStatus(404)
+})
+
+app.delete('/events/:uid', (req, res) => {
+  events.forEach((element, index) => {
+    if (Number(element.uid) === Number(req.params.uid)) {
+      events.splice(index, 1)
+      res.send(events[index])
+    }
+  })
+
+    res.sendStatus(404)
 })
 
 app.listen(port, () => console.log('Listening!'))
